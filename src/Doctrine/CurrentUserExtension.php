267797d5e94e53a19ad78b20c04e9ba35c0b3f45
Sebastian Łuczak <s.luczak@gmi.pl>
2020-04-17 09:21:45 +0200

    Filters Reservation list based on current user.

---
+++ src/Doctrine/CurrentUserExtension.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace App\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Reservation;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
+
+final class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
+    {
+        $this->filterByCurrentUser($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = []): void
+    {
+        $this->filterByCurrentUser($queryBuilder, $resourceClass);
+    }
+
+    private function filterByCurrentUser(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (Reservation::class !== $resourceClass || null === $user = $this->security->getUser()) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.user = :current_user', $rootAlias));
+        $queryBuilder->setParameter('current_user', $user->getId());
+    }
+}
